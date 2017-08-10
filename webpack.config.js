c55506745e0d9ae1e1e65a01a67e18b8e89ce6ff
RHGeek <john.niedzwiecki.ii@gmail.com>
2017-08-09 23:15:23 -0400

    Creates project base. Sets up to use Typescript and Webpack. Adds dev dependencies. Adds example index.html to load bundle.js.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.ts',
+    output: {
+        filename: './dist/tintup-hex.bundle.js'
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            { 
+                test: /\.ts$/,
+                loader: 'ts-loader'
+             }
+        ]
+    }
+}
